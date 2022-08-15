0f4ffc372d794a353ba43ced8c06c7bd1504faf7
MohamedElhadidy <elhadidy.384@gmail.com>
2022-08-15 19:17:41 +0200

    init

--- public/main.js
+++ public/main.js
@@ -13,14 +13,28 @@ async function init() {
   const input = document.getElementById("upload");
   const fileReader = new FileReader();
 
+  let img_data_url = "";
   fileReader.onload = () => {
     let base64 = fileReader.result.replace(
       /^data:image\/(png|jpeg|jpg);base64,/,
       ""
     );
-
-    let img_data_url = rustApp.grayscale(base64);
+    let filter = document.querySelector('input[name="filter"]:checked').value;
+    console.log(filter);
+    if (filter === "grayscale") {
+      img_data_url = rustApp.grayscale(base64);
+    } else if (filter === "blur") {
+      img_data_url = rustApp.blur(base64);
+    } else if (filter === "brighten") {
+      img_data_url = rustApp.brighten(base64);
+    } else if (filter === "flipv") {
+      img_data_url = rustApp.flipv(base64);
+    } else if (filter === "fliph") {
+      img_data_url = rustApp.fliph(base64);
+    }
     document.getElementById("new-img").setAttribute("src", img_data_url);
+    document.getElementById("download-img").setAttribute("href", img_data_url);
+    document.getElementById("download-button").style.display = "block";
   };
 
   input.addEventListener("change", () => {
--- src/lib.rs
+++ src/lib.rs
@@ -30,3 +30,102 @@ pub fn grayscale(encoded_file: &str) -> String{
     data_url
 }
 
+#[wasm_bindgen]
+pub fn blur(encoded_file: &str) -> String{
+    log(&"called".into());
+    let base64_to_vector = decode(encoded_file).unwrap();
+    log(&"Image decoded".into());
+
+    let mut img = load_from_memory(&base64_to_vector).unwrap();
+    log(&"image loaded".into());
+
+    img = img.blur(1.5);
+    log(&"blur effect applied".into());
+
+    let mut buffer = vec![];
+    img.write_to(&mut buffer, Png).unwrap();
+    log(&"new image written".into());
+
+    let encoded_img = encode(&buffer);
+    let data_url = format!(
+        "data:image/png;base64,{}",
+        encoded_img
+    );
+
+    data_url
+}
+#[wasm_bindgen]
+pub fn brighten(encoded_file: &str) -> String{
+    log(&"called".into());
+    let base64_to_vector = decode(encoded_file).unwrap();
+    log(&"Image decoded".into());
+
+    let mut img = load_from_memory(&base64_to_vector).unwrap();
+    log(&"image loaded".into());
+
+    img = img.brighten(50);
+    log(&"brighten effect applied".into());
+
+    let mut buffer = vec![];
+    img.write_to(&mut buffer, Png).unwrap();
+    log(&"new image written".into());
+
+    let encoded_img = encode(&buffer);
+    let data_url = format!(
+        "data:image/png;base64,{}",
+        encoded_img
+    );
+
+    data_url
+}
+
+#[wasm_bindgen]
+pub fn flipv(encoded_file: &str) -> String{
+    log(&"called".into());
+    let base64_to_vector = decode(encoded_file).unwrap();
+    log(&"Image decoded".into());
+
+    let mut img = load_from_memory(&base64_to_vector).unwrap();
+    log(&"image loaded".into());
+
+    img = img.flipv();
+    log(&"flipv effect applied".into());
+
+    let mut buffer = vec![];
+    img.write_to(&mut buffer, Png).unwrap();
+    log(&"new image written".into());
+
+    let encoded_img = encode(&buffer);
+    let data_url = format!(
+        "data:image/png;base64,{}",
+        encoded_img
+    );
+
+    data_url
+}
+
+#[wasm_bindgen]
+pub fn fliph(encoded_file: &str) -> String{
+    log(&"called".into());
+    let base64_to_vector = decode(encoded_file).unwrap();
+    log(&"Image decoded".into());
+
+    let mut img = load_from_memory(&base64_to_vector).unwrap();
+    log(&"image loaded".into());
+
+    img = img.fliph();
+    log(&"fliph effect applied".into());
+
+    let mut buffer = vec![];
+    img.write_to(&mut buffer, Png).unwrap();
+    log(&"new image written".into());
+
+    let encoded_img = encode(&buffer);
+    let data_url = format!(
+        "data:image/png;base64,{}",
+        encoded_img
+    );
+
+    data_url
+}
+
