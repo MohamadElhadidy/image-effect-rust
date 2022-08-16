aed583cb3ebf7e5b2593ba28dc767586456fb27b
unknown <MohamadElhadidy>
2022-08-16 12:15:56 +0200

    Revert "Update main.js"
    This reverts commit f39672d7bbb171ad747041bd8e6c24be92bf9a40.

--- public/main.js
+++ public/main.js
@@ -39,7 +39,6 @@ async function init() {
 
   input.addEventListener("change", () => {
     fileReader.readAsDataURL(input.files[0]);
-    document.getElementById("new-img").setAttribute("src", '');
   });
 }
 
